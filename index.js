2c6a98e32c9bb7714baba8d6a02f4c9b72acea87
Samuel <samuelou510@gmail.com>
2023-06-13 15:07:42 -0400

    completed scope 1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  // const upper = customerName.toUpperCase();
+  // let name = 'bob';
+  customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer(string = 'not bob'){
+  bestCustomer = string;
+}
+
+function overwriteBestCustomer(){
+  setBestCustomer('maybe bob');
+}
+const leastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "g";
+}
